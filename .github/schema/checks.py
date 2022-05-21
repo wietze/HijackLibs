926e70eb1bd0741d91f25dc6951a595bfcd1ef92
Wietze <wietze@users.noreply.github.com>
2022-05-21 21:41:46 +0100

    Adding Environment Variable-based entries

--- .github/schema/checks.py
+++ .github/schema/checks.py
@@ -1,6 +1,20 @@
 import logging
 log = logging.getLogger(__name__)
+log.setLevel("DEBUG")
 
+def check_executables(value, rule_obj, path):
+    log.debug("value: %s", value)
+    log.debug("rule_obj: %s", rule_obj)
+    log.debug("path: %s", path)
+
+    if value.get("Type") == "Environment Variable" and not value.get("Variable"):
+        raise AssertionError("'Variable' must be specified when 'Type' is set to 'Environment Variable'.")
+    if value.get("Type") != "Environment Variable" and value.get("Variable"):
+        raise AssertionError("'Variable' must only be specified if 'Type' is set to 'Environment Variable'.")
+    if value.get("Variable") and '%' in value.get("Variable"):
+        raise AssertionError("'Variable' should contain the Environment Variable name without percentage signs (%).")
+
+    return not_empty(value, rule_obj, path)
 
 def not_empty(value, rule_obj, path):
     log.debug("value: %s", value)
