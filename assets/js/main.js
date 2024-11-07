2d42c96c7f070a581fe6aa5886a8e91ae8c880a2
Wietze <wietze@users.noreply.github.com>
2024-11-07 19:12:45 +0000

    Improvements for mobile rendering

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,26 +20,29 @@ function parseUrls(url) {
                 }
 }
 
-function downloadSigma(target){
-    target.href = 'data:text/yaml;base64,' + btoa(document.querySelector('div.yaml-tab-selected code[data-lang=yml]').textContent);
+function downloadSigma(target) {
+    let a = document.createElement("a"); //Create <a>
+    a.href = "data:text/yaml;base64," + btoa(document.querySelector('div.yaml-tab-selected code[data-lang=yml]').textContent);
+    a.download = target.dataset['filename'] || "sigma.yml"; //File name here
+    a.click(); //Downloaded file
 }
 
-function changeYamlDefinition(target){
+function changeYamlDefinition(target) {
     yaml_selected = target.textContent.toLowerCase();
     // Update tabs
     curentOffset = document.querySelector('.yaml-tab-selected code')?.scrollTop
     document.querySelector('.yaml-tab-selected')?.classList.remove('yaml-tab-selected')
-    document.querySelector('#yaml-'+yaml_selected).classList.add('yaml-tab-selected')
-    document.querySelector('#yaml-'+yaml_selected + ' code').scrollTop = curentOffset
+    document.querySelector('#yaml-' + yaml_selected).classList.add('yaml-tab-selected')
+    document.querySelector('#yaml-' + yaml_selected + ' code').scrollTop = curentOffset
     // Update buttons
     document.querySelector('.yaml-button-selected')?.classList.remove('yaml-button-selected')
     target.classList.add('yaml-button-selected')
     // Update feed url
-    document.querySelector('#yaml-feed-url').href = document.querySelector('#yaml-feed-url').href.replace(/sigma_feed_.*?.yml/i, "sigma_feed_"+yaml_selected+".yml")
+    document.querySelector('#yaml-feed-url').href = document.querySelector('#yaml-feed-url').href.replace(/sigma_feed_.*?.yml/i, "sigma_feed_" + yaml_selected + ".yml")
 }
 
-function goBack(){
-    if(document.referrer && (document.referrer.indexOf("localhost") > 0 || document.referrer.toLowerCase().indexOf("hijacklibs") > 0)){
+function goBack() {
+    if (document.referrer && (document.referrer.indexOf("localhost") > 0 || document.referrer.toLowerCase().indexOf("hijacklibs") > 0)) {
         history.back();
         window.location = "/";
     } else {
@@ -53,15 +56,15 @@ function timeSince(date) {
     var interval = seconds / 31536000;
 
     if (interval > 1) {
-      return  "over " + Math.floor(interval) + " year" + (interval>2 ? "s" : "") + " ago";
+        return "over " + Math.floor(interval) + " year" + (interval > 2 ? "s" : "") + " ago";
     }
     interval = seconds / 2592000;
     if (interval > 1) {
-      return "about " + Math.floor(interval) + " month" + (interval>2 ? "s" : "") + " ago";
+        return "about " + Math.floor(interval) + " month" + (interval > 2 ? "s" : "") + " ago";
     }
     interval = seconds / 86400;
     if (interval > 1) {
-      return Math.floor(interval) + " day" + (interval>2 ? "s" : "") + " ago";
+        return Math.floor(interval) + " day" + (interval > 2 ? "s" : "") + " ago";
     }
     return "Today"
 }
@@ -70,36 +73,44 @@ function isValidGitHubUrl(string) {
     let url;
 
     try {
-      url = new URL(string);
+        url = new URL(string);
     } catch (_) {
-      return false;
+        return false;
     }
 
-    return url.protocol === "https:" && url.hostname==="github.com";
-  }
+    return url.protocol === "https:" && url.hostname === "github.com";
+}
 
 function getLastModified(path) {
-    fetch("https://api.github.com/repos/"+github_repo+"/commits?path="+encodeURIComponent(path), { headers: { "Content-Type": "application/json; charset=utf-8" }})
-    .then(res => res.json())
-    .then(commits => {
-        try {
-            date = commits[0]['commit']['committer']['date'].slice(0,10);
-            url = commits[0]['html_url']
-            if(/^\d{4}-\d{2}-\d{2}$/.test(date) && isValidGitHubUrl(url))
-                document.querySelector('div#last-updated').innerHTML = '<a href="'+url+'" class="link-dotted"><time datetime="'+date+'" title="'+date+'">'+timeSince(new Date(date))+'</time></a>';
+    fetch("https://api.github.com/repos/" + github_repo + "/commits?path=" + encodeURIComponent(path), { headers: { "Content-Type": "application/json; charset=utf-8" } })
+        .then(res => res.json())
+        .then(commits => {
+            try {
+                date = commits[0]['commit']['committer']['date'].slice(0, 10);
+                url = commits[0]['html_url']
+                if (/^\d{4}-\d{2}-\d{2}$/.test(date) && isValidGitHubUrl(url))
+                    document.querySelector('div#last-updated').innerHTML = '<a href="' + url + '" class="link-dotted"><time datetime="' + date + '" title="' + date + '">' + timeSince(new Date(date)) + '</time></a>';
 
-        } catch {
-            console.error("Could not parse JSON")
-        }
-    })
-    .catch(err => {
-        console.error("Could not load JSON:", err);
-    });
+            } catch {
+                console.error("Could not parse JSON")
+            }
+        })
+        .catch(err => {
+            console.error("Could not load JSON:", err);
+        });
 }
 
 
-const load = () => { document.querySelectorAll('a.reference').forEach((x) => { parseUrls(x); });
-if(window.location.pathname.startsWith('/entries/'))
-    getLastModified(window.location.pathname.replace("/entries/", "/yml/").replace(".html", ".yml"))
+const load = () => {
+    document.querySelectorAll('a.reference').forEach((x) => { parseUrls(x); });
+    if (window.location.pathname.startsWith('/entries/'))
+        getLastModified(window.location.pathname.replace("/entries/", "/yml/").replace(".html", ".yml"))
 }
 window.onload = load;
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('button#yaml-download').forEach(x => x.addEventListener("click", _ => downloadSigma(x)))
+    document.querySelectorAll('button.yaml-button').forEach(x => x.addEventListener("click", _ => changeYamlDefinition(x)))
+    document.getElementById('back-button')?.addEventListener("click", _ => goBack())
+});
